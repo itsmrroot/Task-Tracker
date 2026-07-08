b384803ad550ebc3b5ac36a7381d1d63234f0eab
Bashar <salmobeschir@gmail.com>
2026-07-08 16:07:52 +0200

    Task class initialized

--- src/com/company/tasks/Task.java
+++ src/com/company/tasks/Task.java
@@ -1,5 +1,48 @@
 package com.company.tasks;
 
 public class Task {
+    private int id;
+    private String description;
+    private TaskStatus status;
+    private String createdAt;
+    private String updatedAt;
 
+    public Task(int id, String description, String createdAt){
+        this.id = id;
+        this.description = description;
+        this.updatedAt = createdAt;
+        this.status=TaskStatus.TODO;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
