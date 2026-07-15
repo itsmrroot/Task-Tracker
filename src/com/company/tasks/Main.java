6143415958178ab01f7ee4a0c33ca4af1bacd8bf
Bashar <salmobeschir@gmail.com>
2026-07-15 11:18:31 +0200

    main class initialized

--- src/com/company/tasks/Main.java
+++ src/com/company/tasks/Main.java
@@ -3,6 +3,30 @@ package com.company.tasks;
 public class Main {
 
     public static void main(String[] args) {
+        TaskManager manager = new TaskManager();
+
+        switch (args[0]) {
+            case "add":
+                manager.addTask(args[1]);
+                break;
+            case "delete":
+                manager.deleteTask(Integer.parseInt(args[1]));
+                break;
+            case "update":
+                manager.updateTask(Integer.parseInt(args[1]), args[2]);
+                break;
+            case "mark-in-progress":
+                manager.markInProgress(Integer.parseInt(args[1]));
+                break;
+            case "mark-done":
+                manager.markDone(Integer.parseInt(args[1]));
+                break;
+            case "list":
+                manager.listTasks(args.length > 1 ? args[1] : null);
+                break;
+            default:
+                System.out.println("Invalid command.");
+        }
 
 
 
